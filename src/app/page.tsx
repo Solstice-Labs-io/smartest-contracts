0e3c2423d0682cafb5d0d303c200e2e0aa57724a
Carlos <clover@Carloss-MBP.attlocal.net>
2023-07-01 00:44:26 -0400

    Code Review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Example() {
   return (
-    <div className="bg-white">
+    <div className="bg-white h-screen">
       <div className="display isolate px-6 pt-14 lg:px-8">
         {/* <-----Top_Degradation-----> */}
         <div
@@ -14,17 +14,17 @@ export default function Example() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
+
         </div>
         {/* <-----Top_Degradation-----> */}
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-32 sm:py- lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+            <h1 className="font-bold text-gray-900 sm:text-5xl">
+              The Easiest Way of Building Smart Contracts
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Generate, review, and deploy your smart contracts with a few
+              clicks using Artificial Intelligence. No coding required.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
